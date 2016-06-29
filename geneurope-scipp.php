9bcb7eb47315d7a69017e2497c8cc90f94674bbe
Marko Kroflič <marko@mkr.si>
2016-06-29 21:03:23 +0200

    create setting class in separate file fetch categories, evolutions, interactions

--- geneurope-scipp.php
+++ geneurope-scipp.php
@@ -14,11 +14,28 @@ if(!class_exists('WP_SCIPP_Plugin'))
 {
     class WP_SCIPP_Plugin
     {
+        private static $cache_time = MINUTE_IN_SECONDS;
+
+        public $categories;
+        public $evolutions;
+        public $interactions;
+
         /**
          * Construct the plugin object
          */
         public function __construct()
         {
+            // Initialize Settings
+            if( is_admin() ) {
+                require_once(sprintf("%s/settings.php", dirname(__FILE__)));
+                $scipp_settings_page = new ScippSettingsPage();
+            }
+
+            // fill options
+            $this->categories = $this->get_remote_flow("admin/categories");
+            $this->evolutions = $this->get_remote_flow("admin/evolutions");
+            $this->interactions = $this->get_remote_flow("admin/interactions");
+
             // register actions
             //if( !is_admin() ) {
             add_action('wp_enqueue_scripts', array('WP_SCIPP_Plugin', 'enqueued_assets'));
@@ -68,7 +85,7 @@ if(!class_exists('WP_SCIPP_Plugin'))
             if( empty( $data ) ) {
                 $scipp_options = get_option( 'scipp_options' );
                 $api_url = isset( $scipp_options['api_url'] ) ? esc_url( $scipp_options['api_url']) : '';
-                $response = wp_remote_get( $api_url . $path . '.json', array( 'decompress' => false ) );
+                $response = wp_remote_get( $api_url . $path, array( 'decompress' => false, 'headers'     => array( 'Accept' => 'application/json'), ) );
                 if( is_wp_error( $response ) ) {
                     return array();
                 }
@@ -79,7 +96,7 @@ if(!class_exists('WP_SCIPP_Plugin'))
                     return array();
                 }
 
-                set_transient( 'scipp_' . $path , $data, MINUTE_IN_SECONDS );
+                set_transient( 'scipp_' . $path , $data, WP_SCIPP_Plugin::$cache_time );
             }
 
             return $data;
@@ -102,7 +119,7 @@ if(!class_exists('WP_SCIPP_Plugin'))
             ), $atts );
 
             if( $a['show_projects'] ) {
-                $projects = WP_SCIPP_Plugin::get_remote_flow("projects");
+                $projects = WP_SCIPP_Plugin::get_remote_flow("projects.json");
             }
 
             if( empty( $projects ) ) {
@@ -176,7 +193,7 @@ if(!class_exists('WP_SCIPP_Plugin'))
                 'width' => '100%',
             ), $atts );
 
-            $projects = WP_SCIPP_Plugin::get_remote_flow("projects");
+            $projects = WP_SCIPP_Plugin::get_remote_flow("projects.json");
 
             if( empty( $projects ) ) {
                 return;
@@ -255,7 +272,7 @@ if(!class_exists('WP_SCIPP_Plugin'))
                 else
                     $lang = "";
 
-                $path = "projects/" . $lang . "/" . $project_id;
+                $path = "projects/" . $lang . "/" . $project_id . ".json";
 
                 $project = WP_SCIPP_Plugin::get_remote_flow($path);
 
@@ -305,145 +322,3 @@ if(class_exists('WP_SCIPP_Plugin'))
     $wp_scipp_plugin = new WP_SCIPP_Plugin();
 
 }
-
-class ScippSettingsPage
-{
-    /**
-     * Holds the values to be used in the fields callbacks
-     */
-    private $options;
-
-    /**
-     * Start up
-     */
-    public function __construct()
-    {
-        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
-        add_action( 'admin_init', array( $this, 'page_init' ) );
-    }
-
-    /**
-     * Add options page
-     */
-    public function add_plugin_page()
-    {
-        // This page will be under "Settings"
-        add_options_page(
-            'Settings Admin',
-            'SCIPP Settings',
-            'manage_options',
-            'scipp-setting-admin',
-            array( $this, 'create_admin_page' )
-        );
-    }
-
-    /**
-     * Options page callback
-     */
-    public function create_admin_page()
-    {
-        // Set class property
-        $this->options = get_option( 'scipp_options' );
-        ?>
-        <div class="wrap">
-            <h2>SCIPP Settings</h2>
-            <form method="post" action="options.php">
-                <?php
-                // This prints out all hidden setting fields
-                settings_fields( 'scipp_option_group' );
-                do_settings_sections( 'scipp-setting-admin' );
-                submit_button();
-                ?>
-            </form>
-        </div>
-        <?php
-    }
-
-    /**
-     * Register and add settings
-     */
-    public function page_init()
-    {
-        register_setting(
-            'scipp_option_group', // Option group
-            'scipp_options', // Option name
-            array( $this, 'sanitize' ) // Sanitize
-        );
-
-        add_settings_section(
-            'setting_section_id', // ID
-            'SCIPP Custom Settings', // Title
-            array( $this, 'print_section_info' ), // Callback
-            'scipp-setting-admin' // Page
-        );
-
-        add_settings_field(
-            'api_url',
-            'API url',
-            array( $this, 'api_url_callback' ),
-            'scipp-setting-admin',
-            'setting_section_id'
-        );
-
-        /*
-        add_settings_field(
-            'id_number', // ID
-            'ID Number', // Title
-            array( $this, 'id_number_callback' ), // Callback
-            'scipp-setting-admin', // Page
-            'setting_section_id' // Section
-        );
-        */
-
-    }
-
-    /**
-     * Sanitize each setting field as needed
-     *
-     * @param array $input Contains all settings fields as array keys
-     */
-    public function sanitize( $input )
-    {
-        $new_input = array();
-        if( isset( $input['id_number'] ) )
-            $new_input['id_number'] = absint( $input['id_number'] );
-
-        if( isset( $input['api_url'] ) )
-            $new_input['api_url'] = esc_url_raw( $input['api_url'] );
-
-        return $new_input;
-    }
-
-    /**
-     * Print the Section text
-     */
-    public function print_section_info()
-    {
-        print 'Enter your settings below:';
-    }
-
-    /**
-     * Get the settings option array and print one of its values
-     */
-    public function id_number_callback()
-    {
-        printf(
-            '<input type="text" id="id_number" name="scipp_options[id_number]" value="%s" />',
-            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
-        );
-    }
-
-    /**
-     * Get the settings option array and print one of its values
-     */
-    public function api_url_callback()
-    {
-        printf(
-            '<input type="url" id="api_url" name="scipp_options[api_url]" value="%s" class="regular-text code" />',
-            isset( $this->options['api_url'] ) ? esc_url( $this->options['api_url']) : ''
-        );
-    }
-}
-
-if( is_admin() )
-    $scipp_settings_page = new ScippSettingsPage();
---
+++ settings.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marko
+ * Date: 29. 06. 16
+ * Time: 20:37
+ */
+
+class ScippSettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Settings Admin',
+            'SCIPP Settings',
+            'manage_options',
+            'scipp-setting-admin',
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'scipp_options' );
+        ?>
+        <div class="wrap">
+            <h2>SCIPP Settings</h2>
+            <form method="post" action="options.php">
+                <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'scipp_option_group' );
+                do_settings_sections( 'scipp-setting-admin' );
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {
+        register_setting(
+            'scipp_option_group', // Option group
+            'scipp_options', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'SCIPP Custom Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'scipp-setting-admin' // Page
+        );
+
+        add_settings_field(
+            'api_url',
+            'API url',
+            array( $this, 'api_url_callback' ),
+            'scipp-setting-admin',
+            'setting_section_id'
+        );
+
+        /*
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title
+            array( $this, 'id_number_callback' ), // Callback
+            'scipp-setting-admin', // Page
+            'setting_section_id' // Section
+        );
+        */
+
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['api_url'] ) )
+            $new_input['api_url'] = esc_url_raw( $input['api_url'] );
+
+        return $new_input;
+    }
+
+    /**
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="scipp_options[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function api_url_callback()
+    {
+        printf(
+            '<input type="url" id="api_url" name="scipp_options[api_url]" value="%s" class="regular-text code" />',
+            isset( $this->options['api_url'] ) ? esc_url( $this->options['api_url']) : ''
+        );
+    }
+}
