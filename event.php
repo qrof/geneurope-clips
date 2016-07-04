f8ec3a1b51e15ca85c9d57b69e6ab2b3d4a5d33e
Marko Kroflič <marko@mkr.si>
2016-07-04 21:55:47 +0200

    events template output

--- event.php
+++ event.php
@@ -1,7 +1,248 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: marko
- * Date: 26. 06. 16
- * Time: 15:40
+ * The template for displaying a SCIPP event
+ *
  */
+
+get_header(); ?>
+
+<?php
+$e = WP_SCIPP_Plugin::get_event_fromurl();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <?php
+        if ( isset( $e ) && isset( $e->id )) {
+
+            ?>
+            <article id="post-<?php echo $e->id; ?>" <?php post_class("event", null); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php echo $e->properties->name; ?></h1>
+                    <?php
+                    if (isset($e->properties->evolution)) {
+                        ?>
+                    <div class="event-evolution">
+                        <span><?php echo $e->properties->evolution->name; ?></span>
+                    </div>
+                        <?php
+                    }
+                    ?>
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+                    <div id="event-details" class="container">
+                        <div class="row">
+                            <!-- LEFT Column -->
+                            <div class="event-details-left col-md-7 col-sm-7">
+                                <?php
+                                    if (isset($e->properties->start)) {
+                                        ?>
+                                        <div class="event-openingtimes"><p><?php
+                                        echo date_i18n( get_option( 'date_format' ), strtotime( $e->properties->start ) );
+
+                                        if (isset($e->properties->stop)) {
+                                            echo "&nbsp;-&nbsp;" . date_i18n(get_option('date_format'), strtotime($e->properties->stop));
+                                        }
+                                                ?></p>
+                                        </div><?php
+                                    }
+
+                                    if (isset($e->properties->abstract)) {
+                                        ?>
+                                        <div class="clear"></div>
+                                        <div class="event-abstract"><p>
+                                        <?php echo $e->properties->abstract; ?>
+                                        </p></div><?php
+                                    }
+                                    if (isset($e->properties->description)) {
+                                        ?><div class="clear"></div>
+                                    <div class="event-description row">
+                                        <?php echo $e->properties->description; ?>
+                                    </div><?php
+                                    }
+
+                                    $interactions = $e->properties->interactions;
+
+                                    if (!empty($interactions)) {
+                                        $i = "";
+                                        foreach ($interactions as $interaction) {
+                                            $i .= "<span>" . $interaction->name . "</span>, ";
+                                        }
+
+
+                                        $i = rtrim($i, ", ");
+                                        ?>
+                                        <div class="clear"></div>
+                                        <div class="event-interactions row">
+                                            <h4 class="event-interactions-title">Interactions</h4>
+                                            <div><?php echo $i; ?></div>
+                                        </div>
+                                    <?php
+                                    }
+
+                                    $cat_codes = $e->properties->cat_codes;
+
+                                    $c = "";
+
+                                    foreach( WP_SCIPP_Plugin::get_categories() as $category ) {
+                                        if ( in_array($category->id, $cat_codes) ) {
+                                            $c .= "<span>" . $category->name . "</span>, ";
+                                        }
+                                    }
+
+                                    if (!empty($c)){
+                                        $c = rtrim($c, ", ");
+                                        ?>
+                                         <div class="clear"></div>
+                                         <div class="event-categories row">
+                                             <h4 class="event-categories-title">Categories</h4>
+                                             <div><?php echo $c; ?></div>
+                                         </div>
+                                    <?php
+                                    }
+
+                                    $address = $e->properties->address;
+                                ?>
+                                <div class="clear"></div>
+                                <div class="event-address row">
+                                    <h4 class="event-address-title">Address</h4>
+                                    <?php
+                                        if (!empty($address->street)) {
+                                            ?>
+                                            <span class="event-address-1">
+                                                <span><?php echo $address->street; ?></span>
+                                            </span>
+                                            <?php
+                                        }
+
+                                        if (!empty($address->city)) {
+                                            ?>
+                                            <br/><span class="event-address-2">
+                                                <span><?php echo $address->postcode; ?></span>
+                                                <span><?php echo $address->city; ?></span>
+                                            </span>
+                                            <?php
+                                        }
+
+                                        if (!empty($address->country)) {
+                                            ?>
+                                            <br/><span class="event-address-3">
+                                                 <span><?php echo $address->country; ?></span>
+                                             </span>
+                                            <?php
+                                        }
+                                    ?>
+                                </div>
+                                <?php
+                                    $contactRoles = $e->properties->contactRoles;
+                                    if (!empty($contactRoles)){
+                                        ?>
+                                <div class="clear"></div>
+                                <div class="event-contacts row">
+                                    <h4 class="project-contacts-title">Contacts</h4>
+                                    <div class="row contact-role">
+                                        <?php
+                                        $c = "";
+
+                                        foreach( $contactRoles as $contactRole ) { ?>
+                                            <div class="contactrole row">
+                                                <div class="col-xs-5"><?php echo $contactRole->role; ?></div>
+                                                <div style="clear:both"></div>
+                                                <div class="col-xs-7">
+                                                    <?php echo (!empty($contactRole->contact->organisation)) ? "<span><strong>" . $contactRole->contact->organisation . "</strong></span><br/>" : ""; ?>
+                                                    <?php echo (!empty($contactRole->contact->firstname)) ? "<span><strong>" . $contactRole->contact->function . " " . $contactRole->contact->firstname . " " . $contactRole->contact->surename . "</strong></span><br/>" : ""; ?>
+                                                    <?php echo (!empty($contactRole->contact->fon)) ? "<span>Phone: " . $contactRole->contact->fon  . "</span><br/>": ""; ?>
+                                                    <?php echo (!empty($contactRole->contact->mobile)) ? "<span>Mobile: " . $contactRole->contact->mobile  . "</span><br/>": ""; ?>
+                                                    <?php echo (!empty($contactRole->contact->email)) ? "<span>E-mail: <a href=\"" . antispambot( 'mailto:' . $contactRole->contact->email ) . "\">" . antispambot( $contactRole->contact->email ) . "</a></span><br/>": ""; ?>
+                                                </div>
+                                            </div>
+                                            <?php
+                                        }
+                                        ?></div></div>
+                                        <?php
+                                    }
+                                    ?>
+                            </div>
+                            <!-- RIGHT Column-->
+                            <div class="event-details-right col-md-5 col-sm-5">
+                                <?php
+                                if ( !empty($e->properties->project) ) {
+                                ?>
+                                <div class="event-project row">
+                                    <p class="event-project-title">This event is part of the project<br/><a href="<?php echo $e->properties->project->uri; ?>"><?php echo $e->properties->project->name; ?></a></p>
+                                    <div class="col-r-1"><a href="<?php echo $e->properties->project->uri; ?>"><img class="project-image" src="<?php echo $e->properties->project->thumbnail; ?>"/></a></div>
+                                </div>
+                                <?php
+                                }
+                                ?>
+                                <?php
+                                if ( !empty($e->properties->thumbnail) ) {
+                                    ?>
+                                    <div class="col-r-1"><img class="event-image" src="<?php echo $e->properties->thumbnail; ?>"/></div>
+                                    <?php
+                                }
+                                ?>
+                                <div id="event-map"></div>
+                                <script>
+                                    var scipp_event = <?php echo json_encode($e); ?>;
+
+                                    var scipp_event_map = L.map('event-map');
+
+                                    // create the tile layer with correct attribution
+                                    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+                                    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+                                    var osm = new L.TileLayer(osmUrl, {minZoom: 4, maxZoom: 12, attribution: osmAttrib});
+
+                                    // start the map
+                                    var coords;
+                                    for (var i in scipp_event.geometry.geometries ) {
+                                        if ( scipp_event.geometry.geometries[i].type == "Point" ) {
+                                            coords = scipp_event.geometry.geometries[i].coordinates;
+                                            break;
+                                        }
+                                    }
+                                    if ( coords != undefined ) {
+                                        scipp_event_map.setView(new L.LatLng(coords[1],coords[0]),9);
+                                        scipp_event_map.addLayer(osm);
+
+                                        L.geoJson(scipp_event, {
+                                            style: function (feature) {
+                                                return {color: feature.properties.color};
+                                            },
+                                            onEachFeature: function (feature, layer) {
+                                                var popupContent = '<p><strong>' +
+                                                    feature.properties.name + '</strong></p>';
+
+                                                if (feature.properties && feature.properties.abstract) {
+                                                    popupContent += '<p>' + feature.properties.abstract + '</p>';
+                                                }
+
+                                                layer.bindPopup(popupContent);
+                                            }
+                                        }).addTo(scipp_event_map);
+
+                                    }
+
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- .entry-content -->
+
+            </article><!-- #post-## -->
+
+            <?php
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+
+        }
+        ?>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
